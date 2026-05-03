6e8280dde87b1405dc6bc6b2747b031d5bae8858
cw-miller <dylanwickramasinghe3@gmail.com>
2026-05-03 22:55:35 +0530

    - added admin role

--- apps/core/models.py
+++ apps/core/models.py
@@ -2,6 +2,12 @@ from django.db import models
 import uuid
 
 class Profile(models.Model):
+    
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        USER = "USER", "User"
+
+
     profile_id = models.UUIDField(db_index=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=50, unique=True, null=True, blank=True)
     phone = models.CharField(max_length=15, null=True, blank=True)
@@ -9,6 +15,7 @@ class Profile(models.Model):
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    role = models.CharField(max_length=10, choices=Role.choices, default=Role.USER)
 
     class Meta:
         db_table = "profiles"
